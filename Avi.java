9da70783be7cb666e4ecdc3137678e9ada934bfd
JecaRoboticsP <phoenxapi@gmail.com>
2024-10-29 16:54:44 -0500

    Add files via upload

--- Avi.java
+++ Avi.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.Teleop;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 @TeleOp (name = "Avi",group = "Linear OpMode")
@@ -23,19 +24,32 @@ public class Avi extends LinearOpMode {
         motorBR = hardwareMap.get(DcMotor.class, "motorBR");
         motorBL = hardwareMap.get(DcMotor.class, "motorBL");
 
+        motorFR.setDirection(DcMotorSimple.Direction.REVERSE);
+        motorBR.setDirection(DcMotorSimple.Direction.REVERSE);
+
         waitForStart();
         runtime.reset();
 
         while (opModeIsActive()) {
 
             int speed = 1;
+            double y_input = -gamepad1.left_stick_y;
+            double x_input = gamepad1.left_stick_x;
+            double turn = gamepad1.right_stick_x;
 
-            motorFR.setPower((gamepad1.left_stick_y * speed) + (gamepad1.left_stick_x * speed) + gamepad1.right_trigger - gamepad1.left_trigger - (gamepad1.right_stick_x * speed));
-            motorBR.setPower((gamepad1.left_stick_y * speed) - (gamepad1.left_stick_x * speed) - gamepad1.right_trigger + gamepad1.left_trigger - (gamepad1.right_stick_x * speed));
-            motorFL.setPower((gamepad1.left_stick_y * speed) - (gamepad1.left_stick_x * speed) - gamepad1.right_trigger + gamepad1.left_trigger + (gamepad1.right_stick_x * speed));
-            motorBL.setPower((gamepad1.left_stick_y * speed) + (gamepad1.left_stick_x * speed) + gamepad1.right_trigger - gamepad1.left_trigger + (gamepad1.right_stick_x * speed));
+            telemetry.addLine(gamepad1.left_stick_y + " " + gamepad1.left_stick_x);
+            telemetry.update();
+            motorFR.setPower((y_input * speed) - (x_input * speed) - (turn * speed));
+            motorBR.setPower((y_input * speed) + (x_input * speed) - (turn * speed));
+            motorFL.setPower((y_input * speed) + (x_input * speed) + (turn * speed));
+            motorBL.setPower((y_input * speed) - (x_input * speed) + (turn * speed));
 
-
+            /*
+            motorFR.setPower((gamepad1.left_stick_y * speed) - (gamepad1.left_stick_x * speed) + gamepad1.right_trigger - gamepad1.left_trigger + (gamepad1.right_stick_x * speed));
+            motorBR.setPower((gamepad1.left_stick_y * speed) + (gamepad1.left_stick_x * speed) - gamepad1.right_trigger + gamepad1.left_trigger + (gamepad1.right_stick_x * speed));
+            motorFL.setPower(-(gamepad1.left_stick_y * speed) + (gamepad1.left_stick_x * speed) + gamepad1.right_trigger + gamepad1.left_trigger + (gamepad1.right_stick_x * speed));
+            motorBL.setPower(-(gamepad1.left_stick_y * speed) - (gamepad1.left_stick_x * speed) - gamepad1.right_trigger - gamepad1.left_trigger + (gamepad1.right_stick_x * speed));
+            */
         }
     }
 }
